4a4bb720103b57c53a99a4c5fc826f4d07faafe0
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-18 10:59:00 +0700

    menambahkan node

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -6,3 +6,29 @@ struct Node {
 	string name;
 	Node* next;
 };
+
+Node* START = NULL;
+
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukkan NIM: ";
+	cin >> nim;
+	cout << "Masukkan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+}
