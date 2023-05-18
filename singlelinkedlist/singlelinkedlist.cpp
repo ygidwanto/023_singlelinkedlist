d27c261ab54c85dace9209001083879c57682f8f
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-18 11:31:46 +0700

    void treverse

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -89,3 +89,19 @@ bool listEmpty() {
 		return false;
 
 }
+
+void treverse() {
+	if (listEmpty()) {
+		cout << " List Kosong " << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+	else {
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << " NIM: " << currentNode->noMhs << ", Nama:" << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
+}
