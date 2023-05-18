aac7e62c010745a998120e7b754ec52e45a4f0ea
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-18 11:05:55 +0700

    memasukan fungsi bantuan current dan previous

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -31,4 +31,18 @@ void addNode() {
 		START = nodeBaru;
 		return;
 	}
+
+	Node* previous = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
 }
